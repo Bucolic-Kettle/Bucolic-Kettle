bca90e1eaea691c0ed35e5622c71a3ddf5da8c91
aezed <aezedr@gmail.com>
2016-06-04 20:53:48 -0700

    Improve search page styling

--- src/layouts/SearchResults/SearchResults.js
+++ src/layouts/SearchResults/SearchResults.js
@@ -3,10 +3,12 @@ import Header from 'components/Header/Header';
 import styles from './styles.module.css';
 
 function SearchResultsLayout({ children }) {
+  var jumboStyle = { maxHeight: '100px', padding: '10px' };
+
   return (
     <div className="container-fluid">
-      {/*<Header />*/}
-      <div className="jumbotron">
+      <Header />
+      <div className="jumbotron" style={jumboStyle}>
         <h1>Search Results</h1>
       </div>
       <div className="row-fluid">
